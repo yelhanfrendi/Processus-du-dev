63f5c39b2d3a6cb3fa4e5245cc57ba21bf4b7921
yelhanfrendi <101817256+yelhanfrendi@users.noreply.github.com>
2024-03-19 10:01:52 +0100

    version final

--- web_service_traitement_images.py
+++ web_service_traitement_images.py
@@ -35,50 +35,16 @@ def apply_sepia(image):
             sepia_filter.putpixel((px, py), (tr, tg, tb))
     return sepia_filter
 
-def rajouter_text(image):
-    # Superposer le texte sur l'image traitée
-    draw = ImageDraw.Draw(image)
-    # Définir la police et la taille du texte
-    font = ImageFont.truetype("arial.ttf", 20)  # Modifier le chemin de la police si nécessaire
 
-    # Obtenir le texte à superposer sur l'image depuis la zone de saisie de texte
-    texte = st.text_input("Entrez le texte à superposer sur l'image")
-
-    # Option pour la position du texte
-    position_option = st.selectbox("Choisissez la position du texte", ["Haut", "Milieu", "Bas"])
-
-    # Calculer la position du texte en fonction de l'option sélectionnée
-    if position_option == "Haut":
-        position = (10, 10)  # Haut : Position fixe à partir du coin supérieur gauche
-    elif position_option == "Milieu":
-        width, height = image.size
-        position = (10, height // 2)  # Milieu : Verticalement au milieu, à 10 pixels de la gauche
-    elif position_option == "Bas":
-        width, height = image.size
-        position = (10, height - 30)  # Bas : Position fixe à partir du coin inférieur gauche, en supposant que 30 pixels suffisent pour la hauteur du texte
-
-    # Dessiner le texte sur l'image
-    draw.text(position, texte, fill="red", font=font)
-    return image
-
-def reframe_img_cv2(img, scale_percent):
-    """
-    Redimensionne l'image selon un pourcentage donné.
-    
-    Args:
-    - img : image source en format array Numpy
-    - scale_percent : pourcentage du redimensionnement (0.1 pour 10%, 1 pour 100%, etc.)
-    
-    Returns:
-    - Image redimensionnée comme array Numpy
-    """
-    # Calculer les nouvelles dimensions
-    width = int(img.shape[1] * scale_percent)
-    height = int(img.shape[0] * scale_percent)
+def reframe_img_cv2(img):
+    #il faut prendre 4/3 de l'image principal
+    scale_percent = 60
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
     dim = (width, height)
 
-    # Redimensionner l'image
-    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)  
+    # resize image
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)  
     return resized
 
 
@@ -93,6 +59,32 @@ def sauvegarder_image(dossier, image, nom_fichier):
 def apply_blur(image):
     """Appliquer un effet de flou à  l'image."""
     return image.filter(ImageFilter.BLUR)
+
+def rajouter_text(image):
+    # Superposer le texte sur l'image traitée
+    draw = ImageDraw.Draw(image)
+    # Définir la police et la taille du texte
+    font = ImageFont.truetype("arial.ttf", 30)  # Modifier le chemin de la police si nécessaire
+
+    texte = st.text_input("Entrez le texte à superposer sur l'image  (max 100 caractères)")
+
+    # Option pour la position du texte
+    position_option = st.selectbox("Choisissez la position du texte", ["Haut", "Milieu", "Bas"])
+
+    texte_limite = texte[:100] 
+    
+    # Calculer la position du texte en fonction de l'option sélectionnée
+    width, height = image.size
+    if position_option == "Haut":
+        position = (width * 0.5, height * 0.25)  # 25% de la hauteur de l'image
+    elif position_option == "Milieu":
+        position = (width * 0.5, height * 0.5)  # 50% de la hauteur de l'image
+    elif position_option == "Bas":
+        position = (width * 0.50, height * 0.75)  # 75% de la hauteur de l'image
+
+    # Dessiner le texte sur l'image
+    draw.text(position, texte, fill="red", font=font)
+    return image
 
 def crop_center(img, new_width, new_height):
     """
@@ -121,7 +113,18 @@ def crop_center(img, new_width, new_height):
     else:
         return img[starty:starty + new_height, startx:startx + new_width]
 
-
+def create_download_button(image, caption, filename_prefix):
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+    st.download_button(label=f"Télécharger l'image {caption.lower()}",
+                       data=img_byte_arr,
+                       file_name=f'{filename_prefix}_{caption.lower().replace(" ", "_")}_{index}.png',
+                       mime='image/png',
+                       key=f'download_{caption}_{index}')
+    
+if 'image_base' not in st.session_state:
+    st.session_state.image_base = None
 
 st.title('Application de téléchargement et de traitement d\'image')
 
@@ -129,28 +132,28 @@ st.title('Application de téléchargement et de traitement d\'image')
 
 uploaded_file = st.file_uploader("Choisissez une image à  télécharger", type=['jpg', 'png', 'jpeg'])
 
-
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Image originale', use_column_width=True)
+    st.session_state.image_base = Image.open(uploaded_file)
+    st.image(st.session_state.image_base, caption='Image de base', use_column_width=True)
+
+   
     
-    traitement_options = ['Rajouter un texte','Rotation', 'Redimensionner', 'Noir et Blanc', 'Sépia', 'Flou','Découpe']
+if st.session_state.image_base:
+    traitement_options = ['Rajouter un texte', 'Rotation', 'Noir et Blanc', 'Sépia', 'Flou', 'Découpe']
     choix_traitements = st.multiselect('Choisissez les traitements à appliquer:', traitement_options)
+    
 
     if choix_traitements:
-        image_modifiee = image  # Image initiale à modifier
+        image_modifiee = st.session_state.image_base
         
-        for traitement in choix_traitements:
+        for index , traitement in enumerate(choix_traitements):
+            traitement_applique = False
             if traitement == 'Rotation':
                 image_modifiee = rotate_img_cv2(np.array(image_modifiee))
                 image_modifiee = Image.fromarray(image_modifiee)  # Conversion de NumPy array à PIL Image
                 st.image(image_modifiee, caption='Après rotation', use_column_width=True)
-                
-            elif traitement == 'Redimensionner':
-                facteur = st.slider('Choisissez un facteur de redimensionnement:', 0.1, 1.0, 0.5)
-                image_modifiee = reframe_img_cv2(np.array(image_modifiee), facteur)
-                image_modifiee = Image.fromarray(image_modifiee)  # Conversion de NumPy array à PIL Image
-                st.image(image_modifiee, caption='Après redimensionnement', use_column_width=True)
+                traitement_applique = True
+
 
             elif traitement == 'Découpe':
                 pourcentage = st.slider('Choisissez le pourcentage de l\'image à conserver:', 10, 100, 50)
@@ -172,28 +175,38 @@ if uploaded_file is not None:
                 # Après le découpage, convertissez l'image_modifiee en PIL Image pour l'affichage
                 image_modifiee = Image.fromarray(image_modifiee)
                 st.image(image_modifiee, caption='Après conversion en Noir et Blanc', use_column_width=True)
+                traitement_applique = True
+
                 
             elif traitement == 'Noir et Blanc':
                 image_modifiee = convert_to_bw(image_modifiee)
                 st.image(image_modifiee, caption='Après conversion en Noir et Blanc', use_column_width=True)
-                
+                traitement_applique = True
+
             elif traitement == 'Sépia':
                 if isinstance(image_modifiee, np.ndarray):
                     image_modifiee = Image.fromarray(image_modifiee)
                 image_modifiee = apply_sepia(image_modifiee)
-                st.image(image_modifiee, caption='Après application du filtre Sépia', use_column_width=True)  
+                st.image(image_modifiee, caption='Après application du filtre Sépia', use_column_width=True)
+                traitement_applique = True
             elif traitement == 'Flou':
                 image_modifiee = apply_blur(image_modifiee)
                 st.image(image_modifiee, caption='Après application du flou', use_column_width=True)
+                traitement_applique = True
             elif traitement == 'Rajouter un texte':
                 image_modifiee = rajouter_text(image_modifiee)
-                st.image(image_modifiee, caption='Après application de rajouter un texte', use_column_width=True)                
+                st.image(image_modifiee, caption='Après application de rajouter un texte', use_column_width=True) 
+                traitement_applique = True
+
+            if traitement_applique:
+                create_download_button(image_modifiee, traitement, 'traitement')
+           
+            if st.button(f'Utiliser comme nouvelle base après {traitement}', key=f'new_base_{index}'):
+                st.session_state.image_base = image_modifiee
+                
         # Option pour télécharger l'image finale
         img_byte_arr = io.BytesIO()
         image_modifiee.save(img_byte_arr, format='PNG')
         img_byte_arr = img_byte_arr.getvalue()
         
-        st.download_button(label='Télécharger l\'image finale',
-                           data=img_byte_arr,
-                           file_name='image_finale.png',
-                           mime='image/png')
+
